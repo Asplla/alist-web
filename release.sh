5801c7931b885f28ca2518f59d9cfc4fdef7f109
WxHub <wangxu_cn@icloud.com>
2024-09-17 15:44:21 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -6,7 +6,16 @@ cat package.json
 
 # build
 pnpm install
-pnpm i18n:release
++wget https://crowdin.com/backend/download/project/alist/zh-CN.zip 
++unzip zh-CN.zip
++wget https://crowdin.com/backend/download/project/alist/zh-TW.zip 	
++unzip zh-TW.zip
++wget https://crowdin.com/backend/download/project/alist/ja.zip 
++unzip ja.zip
++node ./scripts/i18n.mjs
++rm zh-CN.zip
++rm zh-TW.zip
++rm ja.zip
 pnpm build
 cp -r dist ../
 cd ..
@@ -16,8 +25,8 @@ cd web-dist
 rm -rf dist
 cp -r ../dist .
 git add .
-git config --local user.email "i@nn.ci"
-git config --local user.name "Noah Hsu"
+git config --local user.email "wangxu_cn@icloud.com"
+git config --local user.name "Asplla"
 git commit --allow-empty -m "upload $version dist files" -a
 git tag -a $version -m "release $version"
 cd ..
